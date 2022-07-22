cba70af0c8f5e8949ceee96899b4a00185df9b1d
anhavanakan <106810172+anhavanakan@users.noreply.github.com>
2022-07-22 10:38:02 +0400

    Create Cash.c

---
+++ Cash.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int cents = get_int("Cents: ");
+
+    cents = cents % 100;
+
+    int coin_25 = cents / 25;
+
+    int coin_10 = (cents - 25 * coin_25) / 10;
+
+    int coin_5 = (cents - 25 * coin_25 - 10 * coin_10) / 5;
+
+    int coin_1 = cents - 25 * coin_25 - 10 * coin_10 - 5 * coin_5;
+
+    printf("%i-25cents\n%i-10cents\n%i-5cents\n%i-1cents", coin_25, coin_10, coin_5, coin_1);
+
+
+}
