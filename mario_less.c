7826975d02bc92db725db8f9a37c6862547291a3
anhavanakan <106810172+anhavanakan@users.noreply.github.com>
2022-09-18 21:46:32 +0400

    Update mario_less.c

--- mario_less.c
+++ mario_less.c
@@ -1,47 +1,39 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
-    int height;
 
+    int h;
     do
     {
-
-      height = get_int("Height: ");
-
+        h = get_int("Height: ");
     }
-    while (8 < height || height < 1);
+    while (h < 1 || h > 8);
+    
+    //number of spaces in first raw
+    int space = h - 1;
 
-// for(h) loop / decreasing "height -1"
-    int t = height - 1;
+    //number of hashes in first raw
+    int hash = 1;
 
-// for(j) loop / increasing "1"
-    int a = 1;
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < space; j++)
+        {
+            printf(" ");
+        }
 
-// for(g) loop/ increasing "1"
-    int b = 1;
+        space--;
 
-   for (int i = 0; i < height; i++)
-   {
+        for (int j = 0; j < hash; j++)
+        {
+            printf("#");
+        }
 
-      for (int j = 0; j < a; j++)
-      {
+        hash++;
 
-           for (int h = 0; h < t; h++ )
-           {
-              printf(" ");
-           }
-           t--;
-           for (int g = 0; g < b; g++)
-           {
-               printf("#");
-           }
-           b++;
-
-      }
-      printf("\n");
-
-   }
+        printf("\n");
+    }
 
 }
